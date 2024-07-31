14890ea49f80a80cefd7f8840b860990b6c2e872
Hessel Voortman <hessel@hesselvoortman.nl>
2024-07-31 10:03:22 +0200

    Testing ideas

---
+++ hvec_importers/knmi/knmi.py
@@ -0,0 +1,15 @@
+"""
+Subpackage for downloading and parsing knmi data
+
+Wrapper around Bart Hoekstra's excellent knmy functions
+
+HVEC-lab, 2024
+"""
+
+#from hvec_importers.knmi.knmy import knmy
+
+
+# Station list
+
+
+# Get data by name
