bbff75b6392d2efd60fa80a3e93b701383edb5aa
HVEC-lab <hessel@hesselvoortman.nl>
2026-01-07 11:39:12 +0100

    Debug

--- hvec_importers/sonel/parser.py
+++ hvec_importers/sonel/parser.py
@@ -68,6 +68,6 @@ def parse_sonel_file(filepath: str) -> pd.DataFrame:
 
     # Clean column names
     df.columns = df.columns.str.strip('# ')
-    df.replace(columns = {'Year': 'Time'}, inplace=True)
+    df.rename(columns = {'Year': 'Time'}, inplace=True)
     return df
   
